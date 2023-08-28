f90856119fdef43fcb1752517a2e2f55c1cc4a77
Christian <christiancettour@gmail.com>
2023-08-28 17:52:40 -0300

    Actualización

--- src/componentes/Inicio/Inicio.jsx
+++ src/componentes/Inicio/Inicio.jsx
@@ -1,8 +1,16 @@
-export function Inicio () {
+import "./Inicio.css";
+
+
+export function Inicio() {
 
     return (
-        <div className="container mt-5">
-            <h1>Acá van las noticias</h1>
-        </div>
+        <>
+            <div className="titulo">
+                <h2>BEDELIA</h2>
+            </div>
+            <div className="container mt-5">
+                <h1>Acá van las noticias</h1>
+            </div>
+        </>
     );
 }
