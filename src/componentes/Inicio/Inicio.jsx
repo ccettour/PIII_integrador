02f0dc02170eb54ba454fd0d22dc9d86c9225845
Christian <christiancettour@gmail.com>
2023-08-29 20:52:46 -0300

    Modificación de estilos de Inicio

--- src/componentes/Inicio/Inicio.jsx
+++ src/componentes/Inicio/Inicio.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
 import './Inicio.css';
 
-export function Inicio(){
+export function Inicio() {
   const [noticias, setNoticias] = useState([]);
   const [tema] = useState("Tecnologia");
-  const cantidadNoticias = 11;
+  const cantidadNoticias = 12;
   let pageFinal = cantidadNoticias;
   let pageInicial = 0;
   let temaActual = "Tecnologia";
@@ -25,7 +26,7 @@ export function Inicio(){
     }
 
     const newNoticias = [];
-    for (let i = pageInicial; i < pageFinal; i++) { 
+    for (let i = pageInicial; i < pageFinal; i++) {
       const { title, urlToImage, publishedAt, source, url } = data.articles[i];
       const date = publishedAt.split("T")[0].split("-").reverse().join("-");
       const newItem = {
@@ -42,11 +43,11 @@ export function Inicio(){
   };
 
   const siguiente = () => {
-    pageInicial = pageFinal; 
+    pageInicial = pageFinal;
     pageFinal = pageFinal + cantidadNoticias;
-    fetchNoticias(temaActual); 
+    fetchNoticias(temaActual);
   };
-  
+
   useEffect(() => {
     fetchNoticias(tema);
   }, [tema]);
@@ -54,19 +55,20 @@ export function Inicio(){
     <>
       <div className="titulo">
         <h2>BEDELIA</h2>
-        
+
       </div>
 
       <div className="navbar-toggle" onClick={() => {
         document.querySelector('.navbar-menu').classList.toggle('show');
       }}>
-       
+
       </div>
+
       <div className="container-noticias">
         {noticias.map((noticia, index) => (
           <div key={index} className="item">
             <h2>{noticia.title}</h2>
-            <img src={noticia.urlToImage} alt={noticia.title} />
+            <img src={noticia.urlToImage} alt={noticia.title} className="img-noticia" />
             <div className="info_item">
               <span className="fecha">{noticia.date}</span>
               <span className="fuente">{noticia.sourceName}</span>
@@ -74,7 +76,11 @@ export function Inicio(){
             <a href={noticia.url} target="_blank" rel="noopener noreferrer">Leer más</a>
           </div>
         ))}
-        <span id="btnSiguiente" onClick={siguiente}>Ver más</span>
+      </div>
+      <div className="ver-mas">
+        <Button variant="light" className="btn-light" id="btnSiguiente" onClick={siguiente}>
+          Ver más
+        </Button>
       </div>
     </>
   );
